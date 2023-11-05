8102263542b8950ad88998f22103d3a65f2790c5
Cleiton <cleiton.siqueira.266@gmail.com>
2023-11-05 13:03:42 -0300

    Update Home view

--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -11,5 +11,6 @@ namespace RunGroopWebApp.ViewModels
         public IEnumerable<Club> Clubs { get; set; }
         public string City { get; set; }
         public string State { get; set; }
+        public HomeUserCreateViewModel Register { get; set; } = new HomeUserCreateViewModel();
     }
 }
